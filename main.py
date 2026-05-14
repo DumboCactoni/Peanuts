5409a1a8db101f7efa62d8a3d1c3dbd901abdcf8
DumboCactoni <61648156+DumboCactoni@users.noreply.github.com>
2026-05-13 21:38:46 -0400

    Last Sync: 2026-05-13 21:38 (Mobile)

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 from collections import defaultdict
-sys.stdin = open("main.in","r")
+sys.stdin = open("/Github/main.in","r")
 a = sys.stdin.read().strip().split('\n')
 b  = [i for i in a[0]]
 c = defaultdict(int)
@@ -17,14 +17,13 @@ h = 0
 
 m = int(min(e[0]//d[0], e[1]//d[1], e[2]//d[2]))
 p = int(max(e[0]//d[0], e[1]//d[1], e[2]//d[2]))
-h += m
-#print(m,p)
+print(m,p)
 for i in range(m+1,p+2):
     n = ((i-m)*d[0])*f[0] + ((i-m)*d[1])*f[1] + ((i-m)*d[2])*f[2]
     if n-g>0.3:
         h += i-1
         print(h)
-        #print(n,g)
+        print(n,g)
         break
 else:
     #print(h)
