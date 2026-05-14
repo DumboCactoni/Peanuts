f9f8890e05edd64a4788e5ebf20a4b733d464298
DumboCactoni <61648156+DumboCactoni@users.noreply.github.com>
2026-05-14 17:48:29 -0400

    Last Sync: 2026-05-14 17:48 (Mobile)

--- hamburgers.py
+++ hamburgers.py
@@ -1,6 +1,6 @@
 import sys
 from collections import defaultdict
-#sys.stdin = open("main.in","r")
+#sys.stdin = open("/storage/emulated/0/Github/main.in","r")
 a = sys.stdin.read().strip().split('\n')
 b  = [i for i in a[0]]
 c = defaultdict(int)
@@ -32,6 +32,6 @@ for i in range(m+1,p+2): # i iterates across number of burgers
 #print(h)
 g -= n
 h += p+1
-r = e[0]*f[0] + e[1]*f[1] + e[2]*f[2]
+r = f[0] + f[1] + f[2]
 h += g // r
 print(h)
