5114f866f1a007104915281880af8a6242f50a71
DumboCactoni <61648156+DumboCactoni@users.noreply.github.com>
2026-05-30 20:06:34 +0000

    uoggo

--- maze.py
+++ maze.py
@@ -1,11 +1,12 @@
 import sys
 sys.stdin = open("main.in","r")
 a = sys.stdin.read().strip().split('\n')
-b = [int(i) for i in a[0]]; a=a[1:]
+b = [int(i) for i in a[0].split()]; a=a[1:]
 d = []
 for i in range(len(a)):
     c = a[i].split()
     for j in range(len(c)):
         if c[j] == ".":
             d.append([i,j])
-print(d)
+for i in d:
+    
