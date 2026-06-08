fc6ae5d02dde0c7c697d03e08cd00c83c18b64de
DumboCactoni <61648156+DumboCactoni@users.noreply.github.com>
2026-06-08 17:55:55 +0000

    rotting

--- rowgcd.py
+++ rowgcd.py
@@ -6,7 +6,7 @@ b = [int(i) for i in a[1].split()]; d = []
 c = [int(i) for i in a[2].split()]; e = []
 for i in range(1,len(b)):
     d.append(b[i]-b[0])
-d = math.gcd(d)
+d = math.gcd(*d)
 for i in c:
-    e.append(math.gcd(d,i))
-print(e)
+    e.append(math.gcd(d,i+b[0]))
+print(" ".join([str(i) for i in e]))
