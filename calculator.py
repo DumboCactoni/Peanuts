5210bf333929498954049d4168e6e85d8ed5b305
PeanutWaffles <jiashenli40@gmail.com>
2026-08-19 11:12:00 -0400

    08-19-2026 11:12:00

--- calculator.py
+++ calculator.py
@@ -13,9 +13,10 @@ def fpow(x,y,z): return((x/y)**z)
 def c(x,y): return m.comb(x,y)
 def xcl(x): return m.factorial(x)
 def sum(x,y,z): return ((y-x)//z+1)*(x+y)/2
+def rad(x): return x*m.pi/180
+def deg(x): return x*180/m.pi
 pi = m.pi
 gc = 6.67e-11
-ge = 9.81
 ms = 1.989e30
 yr = 365*24*3600
 au = 1.496e11
@@ -24,8 +25,8 @@ me = 9.109e-31
 mn = 1.675e-27
 qe = 1.602e-192
 pc = 3.086e16
-cv = 3e8
 ls = 3.828e26
+tb = 13.8e9*yr
 def equations(x):
 	return (
 	x**3-3*x**2+7*x-13)
@@ -38,7 +39,8 @@ def factors(num):
             factors.append(num//indice)
     return factors
 
-a = 10**(frac(-26,74,-2.5))
-x = fpow(a*5.8e9*ls)
+a = 9.81*sin(10)-0.4*9.81*cos(10)
+b = 0.4*9.81*cos(10)*1e-3*500**2
+x=acos(frac(a**2,b))/500
 
 print(f"{x:e}")
